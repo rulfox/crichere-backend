01c2881d8eebc801296ef3e60059fca639d62d24
Aswin <aswincodename47@gmail.com>
2026-04-24 20:31:42 +0530

    chore: update Spring Boot to 3.3.5 and Kotlin to 2.0.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    kotlin("jvm") version "1.9.23" apply false
-    id("org.springframework.boot") version "3.2.4" apply false
+    kotlin("jvm") version "2.0.21" apply false
+    id("org.springframework.boot") version "3.3.5" apply false
 }
--- crichere-backend/build.gradle.kts
+++ crichere-backend/build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.2.4"
-    id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.23"
-    kotlin("plugin.spring") version "1.9.23"
-    kotlin("plugin.jpa") version "1.9.23"
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.spring") version "2.0.21"
+    kotlin("plugin.jpa") version "2.0.21"
 }
 
 group = "com.crichere"
